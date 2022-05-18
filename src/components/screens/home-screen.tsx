c5b544a4817ff7735c4ec98dde08d108b7d7bb35
BruninTI <bruno.araujo@viannasempre.com.br>
2022-05-17 21:57:32 -0300

    feat: add bookgrid on home screen

--- src/components/screens/home-screen.tsx
+++ src/components/screens/home-screen.tsx
@@ -1,33 +1,94 @@
 import React from "react";
-import LibraryCard from "../atoms/library-card";
+import { BookType } from "../../types/book-type";
+import Modal from "../atoms/modal";
 import WebPage from "../atoms/webpage";
 import SideMenu from "../molecules/side-menu";
+import BookGrid from "../organisms/book-grid";
 
 export interface HomeScreenProps {}
 const HomeScreen: React.FC<HomeScreenProps> = () => {
+  const [showBookDetailModal, setShowBookDetailModal] = React.useState(false);
+
   return (
-    <WebPage className="w-full h-full flex">
+    <WebPage className="w-full h-full flex ">
       <div className="w-full max-w-[15rem]">
         <SideMenu />
       </div>
-      <div className="flex flex-col w-full">
-        <h1 className="text-medium text-indigo-700 text-3xl px-24 pt-12">
+      <div className="flex flex-col w-full px-8 ">
+        <h1 className="text-medium text-indigo-700 text-3xl pt-12">
           Biblioteca
         </h1>
         <div>
-          <p className="text-medium text-gray-600 opacity-40 text-xl px-24 py-12">
+          <p className="text-medium text-gray-600 opacity-40 text-xl py-12">
             Ficção e Magia
           </p>
         </div>
-        <LibraryCard
-          title="Bruno"
-          description="Lorem Ipsum is simply dummy text of the printing and typesetting industry.  "
-          src="https://avatars.githubusercontent.com/u/76910959?v=4"
-          alt="Bruno"
+        <BookGrid
+          books={booksList}
+          openBookDetail={() => setShowBookDetailModal(true)}
         />
       </div>
+      <Modal
+        isOpen={showBookDetailModal}
+        closeModal={() => setShowBookDetailModal(false)}
+      >
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          Hello World
+        </div>
+      </Modal>
     </WebPage>
   );
 };
 
 export default HomeScreen;
+
+const booksList = [
+  {
+    id: "harry_potter",
+    title: "Harry Potter e a Pedra Filosofal",
+    description: "A história de Harry Potter e a Pedra Filosofal",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41897yAI4LL._SX346_BO1,204,203,200_.jpg",
+    coverAlt: "Harry Potter e a Pedra Filosofal",
+  },
+  {
+    id: "harry_pot",
+    title: "Harry Potter e a Pedra Filosofal",
+    description: "A história de Harry Potter e a Pedra Filosofal",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41897yAI4LL._SX346_BO1,204,203,200_.jpg",
+    coverAlt: "Harry Potter e a Pedra Filosofal",
+  },
+  {
+    id: "harry_po",
+    title: "Harry Potter e a Pedra Filosofal",
+    description: "A história de Harry Potter e a Pedra Filosofal",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41897yAI4LL._SX346_BO1,204,203,200_.jpg",
+    coverAlt: "Harry Potter e a Pedra Filosofal",
+  },
+  {
+    id: "harry_p",
+    title: "Harry Potter e a Pedra Filosofal",
+    description: "A história de Harry Potter e a Pedra Filosofal",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41897yAI4LL._SX346_BO1,204,203,200_.jpg",
+    coverAlt: "Harry Potter e a Pedra Filosofal",
+  },
+  {
+    id: "harry_",
+    title: "Harry Potter e a Pedra Filosofal",
+    description: "A história de Harry Potter e a Pedra Filosofal",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41897yAI4LL._SX346_BO1,204,203,200_.jpg",
+    coverAlt: "Harry Potter e a Pedra Filosofal",
+  },
+  {
+    id: "harry",
+    title: "Harry Potter e a Pedra Filosofal",
+    description: "A história de Harry Potter e a Pedra Filosofal",
+    coverUrl:
+      "https://images-na.ssl-images-amazon.com/images/I/41897yAI4LL._SX346_BO1,204,203,200_.jpg",
+    coverAlt: "Harry Potter e a Pedra Filosofal",
+  },
+] as BookType[];
