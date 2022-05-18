3618528f225725545ef000f9b9185606c62d8858
BruninTI <bruno.araujo@viannasempre.com.br>
2022-05-18 20:48:06 -0300

    feat: create add books route

---
+++ src/pages/adicionar/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import BookFormScreen from "../../components/screens/book-form-screen";
+
+export interface BookFormScreenRouteProps {}
+const BookFormScreenRoute: React.FC<BookFormScreenRouteProps> = () => {
+  return <BookFormScreen />;
+};
+
+export default BookFormScreenRoute;
