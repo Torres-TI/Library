4670ff45d5f108525fa51c14a9e4ee0f2d678038
BruninTI <bruno.araujo@viannasempre.com.br>
2022-05-17 21:58:04 -0300

    feat: create book grid component

---
+++ src/components/organisms/book-grid.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BookType } from "../../types/book-type";
+import LibraryCard from "../atoms/library-card";
+
+export interface BookGridProps {
+  books: BookType[];
+  openBookDetail: () => void;
+}
+const BookGrid: React.FC<BookGridProps> = ({ books, openBookDetail }) => {
+  return (
+    <div className="  grid gap-x-2 gap-y-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4  ">
+      {books.map((book) => {
+        return (
+          <LibraryCard key={book.id} book={book} onClick={openBookDetail} />
+        );
+      })}
+    </div>
+  );
+};
+
+export default BookGrid;
