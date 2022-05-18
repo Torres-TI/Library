630e448a8cfa386ba9f8cc4bd64d8718f0d684ac
BruninTI <bruno.araujo@viannasempre.com.br>
2022-05-18 20:48:18 -0300

    feat: create book service

---
+++ src/services/create-book-service.ts
@@ -0,0 +1,34 @@
+import { addDoc, collection } from "firebase/firestore";
+import { db } from "../../firebase";
+import { BookType } from "../types/book-type";
+
+type CreateBookServiceType = {
+  book: BookType;
+};
+const CreateBookService = async ({ book }: CreateBookServiceType) => {
+  const bookRef = await collection(db, "books");
+  const bookData = {
+    title: book.title,
+    author: book.author,
+    description: book.description,
+  };
+  try {
+    const bookDoc = await addDoc(bookRef, bookData);
+    const bookDetailsRef = await collection(db, "books", bookDoc.id, "details");
+    const addBook = await addDoc(bookDetailsRef, {
+      title: book.title,
+      description: book.description,
+      coverUrl: book.coverUrl,
+      coverAlt: book.coverAlt,
+      author: book.author,
+      cdd: book.cdd,
+      cdu: book.cdu,
+      edition: book.edition,
+    });
+    return addBook;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+export default CreateBookService;
