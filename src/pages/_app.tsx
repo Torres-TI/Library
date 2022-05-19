f1407da6e0596f99cf4756a465c66f25b6a65225
BruninTI <bruno.araujo@viannasempre.com.br>
2022-05-19 00:57:22 -0300

    feat: add auth provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from "next/app";
 import { QueryClient, QueryClientProvider } from "react-query";
 import "../../styles/globals.css";
+import { AuthProvider } from "../context/auth-context";
 const queryClient = new QueryClient();
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Component {...pageProps} />
+      <AuthProvider>
+        <Component {...pageProps} />
+      </AuthProvider>
     </QueryClientProvider>
   );
 }
