2a2c173e540a906da750fb42dfdc00b22815cc50
BruninTI <bruno.araujo@viannasempre.com.br>
2022-05-20 00:21:49 -0300

    feat: add improvements on home screen

--- src/components/screens/home-screen.tsx
+++ src/components/screens/home-screen.tsx
@@ -2,10 +2,9 @@ import React from "react";
 import useGetBookById from "../../api-hooks/use-get-book-by-id";
 import useGetBooks from "../../api-hooks/use-get-books";
 import { BookType } from "../../types/book-type";
-import WebPage from "../atoms/webpage";
-import SideMenu from "../molecules/side-menu";
 import BookDetailModal from "../organisms/book-detail-modal";
 import BookGrid from "../organisms/book-grid";
+import PrivateTemplate from "../template/private-template";
 
 export interface HomeScreenProps {
   book: BookType;
@@ -21,33 +20,20 @@ const HomeScreen: React.FC<HomeScreenProps> = ({ book }) => {
   const bookDetail = data || [];
 
   return (
-    <WebPage className="w-full h-full flex ">
-      <div className="w-full max-w-[15rem]">
-        <SideMenu />
-      </div>
-      <div className="flex flex-col w-full px-8 ">
-        <h1 className="text-medium text-indigo-700 text-3xl pt-12">
-          Biblioteca
-        </h1>
-        <div>
-          <p className="text-medium text-gray-600 opacity-40 text-xl py-12">
-            Ficção e Magia
-          </p>
-        </div>
-        <BookGrid
-          books={getBooks!}
-          onBookClick={(bookId) => {
-            setBookId(bookId);
-            setShowBookDetailModal(true);
-          }}
-        />
-      </div>
+    <PrivateTemplate title="Biblioteca">
+      <BookGrid
+        books={getBooks!}
+        onBookClick={(bookId) => {
+          setBookId(bookId);
+          setShowBookDetailModal(true);
+        }}
+      />
       <BookDetailModal
+        booksDetail={bookDetail}
         isOpen={showBookDetailModal}
         closeModal={() => setShowBookDetailModal(false)}
-        booksDetail={bookDetail}
       />
-    </WebPage>
+    </PrivateTemplate>
   );
 };
 
