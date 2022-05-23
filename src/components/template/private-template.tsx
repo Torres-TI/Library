afcf50b1f057bc31243da99c7a390d0a044c78fd
BruninTI <bruno.araujo@viannasempre.com.br>
2022-05-22 23:05:13 -0300

    feat: add container on private template

--- src/components/template/private-template.tsx
+++ src/components/template/private-template.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Container from "../atoms/container";
 import Body from "../molecules/body";
 import SideMenu from "../molecules/side-menu";
 import Header from "../organisms/header";
@@ -15,8 +16,10 @@ const PrivateTemplate: React.FC<PrivateTemplateProps> = ({
     <div className="flex h-scree w-screen">
       <SideMenu />
       <div className="flex flex-col w-full p-7">
-        <Header title={title} />
-        <Body>{children}</Body>
+        <Container>
+          <Header title={title} />
+          <Body>{children}</Body>
+        </Container>
       </div>
     </div>
   );
