2c2fe0a4399850389fc46da90a32de304d72f65e
BruninTI <bruno.araujo@viannasempre.com.br>
2022-05-22 23:04:53 -0300

    feat: remove web page

--- src/components/screens/authentications/login-screen.tsx
+++ src/components/screens/authentications/login-screen.tsx
@@ -1,16 +1,15 @@
 import React from "react";
 import Card from "../../atoms/card";
-import WebPage from "../../atoms/webpage";
 import LoginForm from "./login-form";
 
 export interface LoginScreenProps {}
 const LoginScreen: React.FC<LoginScreenProps> = () => {
   return (
-    <WebPage className="relative flex flex-col flex-1 items-center justify-center min-w-screen min-h-screen ">
+    <div className="relative flex flex-col flex-1 items-center justify-center min-w-screen min-h-screen ">
       <Card className="flex flex-col items-center px-4 md:px-6 py-4  w-full  max-w-[22rem] md:max-w-[35rem]">
         <LoginForm />
       </Card>
-    </WebPage>
+    </div>
   );
 };
 
